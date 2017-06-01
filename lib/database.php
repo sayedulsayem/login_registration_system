c4f1d12501436fda9256c8827ac878722a863269
sayedul sayem <sayedulsayem@yahoo.com>
2017-06-02 00:54:34 +0600

    connection class complete

--- lib/database.php
+++ lib/database.php
@@ -1,8 +1,24 @@
 <?php
 
 class database {
+    private $hostdb="localhost";
+    private $userdb="root";
+    private $passdb="";
+    private $namedb="login_registration";
+    private $pdo;
     public function __construct()
     {
+        if(!isset($this->pdo)){
+            try{
+                $link=new PDO("mysql:host=$this->hostdb;dbname=$this->namedb", $this->userdb, $this->passdb);
+                $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $link->exec("SET CHARACTER SET utf8");
+                $this->pdo= $link;
+            }
+            catch (PDOException $e){
+                die("Failed to connect with database".$e->getMessage());
+            }
+        }
 
     }
 }
